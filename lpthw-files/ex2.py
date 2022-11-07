72a5cc7f0bd84d0a388b332f111b31d8edbd1cbd
Garrett Smith <lamelyte@gmail.com>
2022-11-06 20:27:58 -0600

    exercise 2

---
+++ lpthw-files/ex2.py
@@ -0,0 +1,7 @@
+# This is an exercise about comments. Let's try it about
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
