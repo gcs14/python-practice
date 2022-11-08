e143011ec26a4c097599bc30c292034163270c0d
Garrett Smith <lamelyte@gmail.com>
2022-11-07 20:34:43 -0600

    exercise 3

---
+++ lpthw-files/ex3.py
@@ -0,0 +1,26 @@
+# Let's make a program to count chickens
+print("I will now count my chickens:")
+
+# Count up the different sex of the chickens
+print("Hens", float(25 + 30 / 6))
+print("Roosters", float(100 - 25 * 3 % 4))
+
+#  How many eggs do we have?
+print("Now I will count the eggs:")
+#  Add the up
+print(3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
+# Ask the prompt
+print ("Is it true that 3 + 2 < 5 - 7?")
+# Is it True or False
+print(3 + 2 < 5 - 7)
+# Simple arithmatic
+print("What is 3 + 2?", float(3 + 2))
+print("What is 5 - 7?", float(5 - 7))
+# I get it now!!!
+print("Oh, that's why it's False.")
+# Another transition
+print("How about some more?")
+# Speed comparison. Is it greater, lesser, or equal?
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
